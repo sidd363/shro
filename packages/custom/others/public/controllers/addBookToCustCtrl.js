39a6cce9a8266dc66bdab213b10f41de4e3fe969
Siddhant <siddhant@silverpush.co>
2017-07-29 13:30:55 +0530

    gg

--- packages/custom/others/public/controllers/addBookToCustCtrl.js
+++ packages/custom/others/public/controllers/addBookToCustCtrl.js
@@ -14,26 +14,37 @@ angular.module('mean.others').controller('AddBookToCustCtrl', ['$scope', 'Global
                 $scope.bookArr = [];
             })
         }
-        $scope.addToMyAccount = function(book){
-            console.log("book to be added==>>",book.title);
+        function alreadyAvailed(book){
+            var toReturn=true;
             if($scope.global.user && $scope.global.user.booksAssigned){
                 for(var i=0;i<$scope.global.user.booksAssigned.length;i++){
                     if($scope.global.user.booksAssigned[i]==book._id){
-                        console.log("book to be added==>>",book.title);
-                        alert("This Book is already in your account");
-                        return;
+                        return toReturn=false;
+                        break;
                     }
                     console.log("$scope.global.user.booksAssigned[i]d==>>",$scope.global.user.booksAssigned[i]);
                 }
             }
+            return toReturn;
+        }
+        $scope.addToMyAccount = function(book){
+            console.log("book to be added==>>",book.title);
             
-            $http.post('/api/addBookToCustomer',{userid:$scope.global.user._id,bookToBeAdded:book._id})
-            .success(function(res){
-                console.log("all books got", res);
-                $location.url('/');
-            }).error(function(error){
-                console.log("error in adding book to my account", error);
-            })
+            var check = alreadyAvailed(book);
+            console.log("check value", check);
+            if(check){
+                $http.post('/api/addBookToCustomer',{userid:$scope.global.user._id,bookToBeAdded:book._id})
+                .success(function(res){
+                    console.log("all books got", res);
+                    $location.url('/');
+                }).error(function(error){
+                    console.log("error in adding book to my account", error);
+                })
+            } else{
+                console.log("book to be added==>>",book.title);
+                alert("This Book is already in your account");
+            }
+            
         }
     
     }
